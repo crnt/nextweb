4bd35a3dab0fa99af1b1defee9e1d298658ccacf
highpower <highpower@yandex-team.ru>
2010-12-20 18:36:14 +0300

    developing converters

--- include/nextweb/utils/StringConverters.hpp
+++ include/nextweb/utils/StringConverters.hpp
@@ -29,13 +29,39 @@
 
 #include "nextweb/utils/Range.hpp"
 #include "nextweb/utils/Integer.hpp"
-#include "nextweb/utils/SystemError.hpp"
 
 namespace nextweb { namespace utils {
 
 class ConvertError : public Error {
 public:
 	ConvertError();
+	static void throwUnless(bool condition);
+};
+
+template <bool IsSigned>
+struct IntConverter;
+
+template <>
+struct IntConverter<true> {
+	static IntMax fromCharPtr(char const *value);
+};
+
+template <>
+struct IntConverter<false> {
+	static UIntMax fromCharPtr(char const *value);
+};
+
+template <typename X, bool IsInteger>
+struct CharPtrConverter;
+
+template <typename X>
+struct CharPtrConverter<X, true> {
+	static X fromCharPtr(char const *val);
+};
+
+template <typename X>
+struct CharPtrConverter<X, false> {
+	static X fromCharPtr(char const *val);
 };
 
 template <typename X, typename String, bool IsInteger>
@@ -44,28 +70,71 @@ struct StringConverter;
 template <typename X, typename String>
 struct StringConverter<X, String, true>  {
 	static String toString(X val);
+	static X fromString(String const &val);
 };
 
 template <typename X, typename String>
 struct StringConverter<X, String, false>  {
 	static String toString(X val);
+	static X fromString(String const &val);
 	typedef std::basic_stringstream<typename String::value_type> StreamType;
+};
+
+template <typename String>
+struct StringConverter<String, String, false> {
+	static String toString(String const &val);
+	static String fromString(String const &val);
 };
 
 template <typename String>
 struct StringConverter<IntMax, String, true> {
 	static String toString(IntMax val);
+	static IntMax fromString(String const &val);
 };
 
 template <typename String>
 struct StringConverter<UIntMax, String, true> {
 	static String toString(UIntMax val);
+	static UIntMax fromString(String const &val);
 };
+
+template <typename X> X
+fromCharPtr(char const *value);
+
+template <typename X> NEXTWEB_INLINE X
+CharPtrConverter<X, true>::fromCharPtr(char const *val) {
+	typedef typename MaxInt<X>::Type IntType;
+	IntType res = IntConverter<std::numeric_limits<X>::is_signed>::fromCharPtr(val);
+	if (static_cast<IntType>(std::numeric_limits<X>::max()) < res) {
+		throw ConvertError();
+	}
+	return res;
+}
+
+template <typename X> NEXTWEB_INLINE X
+CharPtrConverter<X, false>::fromCharPtr(char const *val) {
+	try {
+		X res;
+		std::stringstream stream;
+		stream.exceptions(std::ios::failbit);
+		stream << val;
+		stream >> res;
+		return res;
+	}
+	catch (std::ios::failure const &) {
+		throw ConvertError();
+	}
+}
 
 template <typename X, typename String> NEXTWEB_INLINE String
 StringConverter<X, String, true>::toString(X val) {
 	typedef typename MaxInt<X>::Type ValueType;
 	return StringConverter<ValueType, String, true>::toString(static_cast<ValueType>(val));
+};
+
+template <typename X, typename String> NEXTWEB_INLINE X
+StringConverter<X, String, true>::fromString(String const &val) {
+	return fromCharPtr<X>(val.c_str());
 };
 
 template <typename X, typename String> NEXTWEB_INLINE String
@@ -83,23 +152,63 @@ StringConverter<X, String, false>::toString(X val) {
 	}
 };
 
+template <typename X, typename String> NEXTWEB_INLINE X
+StringConverter<X, String, false>::fromString(String const &val) {
+	try {
+		X res;
+		StreamType stream;
+		stream.exceptions(std::ios::badbit);
+		stream << val;
+		stream >> res;
+		return res;
+	}
+	catch (std::ios::failure const &) {
+		throw ConvertError();
+	}
+}
+
+template <typename String> NEXTWEB_INLINE String
+StringConverter<String, String, false>::toString(String const &val) {
+	return val;
+}
+
+template <typename String> NEXTWEB_INLINE String
+StringConverter<String, String, false>::fromString(String const &val) {
+	return val;
+}
+
 template <typename String> NEXTWEB_INLINE String
 StringConverter<IntMax, String, true>::toString(IntMax val) {
 	char buf[64];
-	int res = snprintf(buf, sizeof(buf), "%lld", val);
-	SystemError::throwUnless(res >= 0);
-	Range<char const*> range(buf, buf + res);
+	int success = snprintf(buf, sizeof(buf), "%lld", val);
+	ConvertError::throwUnless(success > 0);
+	Range<char const*> range(buf, buf + success);
 	return String(range.begin(), range.end());
 };
+
+template <typename String> NEXTWEB_INLINE IntMax
+StringConverter<IntMax, String, true>::fromString(String const &val) {
+	return fromCharPtr<IntMax>(val.c_str());
+}
+
+template <typename String> NEXTWEB_INLINE UIntMax
+StringConverter<UIntMax, String, true>::fromString(String const &val) {
+	return fromCharPtr<UIntMax>(val.c_str());
+}
 
 template <typename String> NEXTWEB_INLINE String
 StringConverter<UIntMax, String, true>::toString(UIntMax val) {
 	char buf[64];
-	int res = snprintf(buf, sizeof(buf), "%llu", val);
-	SystemError::throwUnless(res >= 0);
-	Range<char const*> range(buf, buf + res);
+	int success = snprintf(buf, sizeof(buf), "%llu", val);
+	ConvertError::throwUnless(success > 0);
+	Range<char const*> range(buf, buf + success);
 	return String(range.begin(), range.end());
 };
+
+template <typename X> NEXTWEB_INLINE X
+fromCharPtr(char const *val) {
+	return CharPtrConverter<X, IsInt<X>::RESULT>::fromCharPtr(val);
+}
 
 template <typename X> NEXTWEB_INLINE std::string
 toString(X value) {
@@ -109,6 +218,16 @@ toString(X value) {
 template <typename String, typename X> NEXTWEB_INLINE String
 toString(X value) {
 	return StringConverter<X, String, IsInt<X>::RESULT>::toString(value);
+}
+
+template <typename X> NEXTWEB_INLINE X
+fromString(char const *val) {
+	return fromCharPtr<X>(val);
+}
+
+template <typename X, typename String> NEXTWEB_INLINE X
+fromString(String const &val) {
+	return StringConverter<X, String, IsInt<X>::RESULT>::fromString(val);
 }
 
 }} // namespaces
