62da8b9f70a83e373ee18da366748ec4bba79645
highpower <highpower@yandex-team.ru>
2011-01-12 16:23:16 +0300

    none

---
+++ tests/fastcgi/ResponseTest.cpp
@@ -0,0 +1,45 @@
+#include "acsetup.hpp"
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "MockIO.hpp"
+#include "nextweb/fastcgi/impl/GenericResponse.hpp"
+
+namespace nextweb { namespace tests {
+
+class ResponseTest : public CppUnit::TestFixture {
+
+public:
+	void testHeaders();
+	void testCookies();
+	
+private:
+	CPPUNIT_TEST_SUITE(ResponseTest);
+	CPPUNIT_TEST(testCookies);
+	CPPUNIT_TEST(testHeaders);
+	CPPUNIT_TEST_SUITE_END();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(ResponseTest);
+
+void
+ResponseTest::testHeaders() {
+
+	using namespace fastcgi;
+	MockIO io;
+	GenericResponse<MockIO> response(io);
+	
+	
+}
+
+void
+ResponseTest::testCookies() {
+
+	using namespace fastcgi;
+	MockIO io;
+	GenericResponse<MockIO> response(io);
+}
+
+
+}} // namespaces
